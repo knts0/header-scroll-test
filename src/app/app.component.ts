2265f41fad46bb7827f33ce1e75bd5f4aa3b1361
Kana Otawara <shimmer22.0204@gmail.com>
2021-04-09 02:05:40 +0900

    first implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sp-playground';
+
+  HEADER_HEIGHT: number = 60;
+  private startPos: number = 0;
+  headerPos: number = 0;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  @HostListener('scroll', ['$event'])
+  onScroll(event) {
+    console.log('aaas')
+    let currentPos = this.document.body.scrollTop;
+    if(currentPos > this.startPos) {
+      if(this.document.body.scrollTop >= this.HEADER_HEIGHT) {
+        this.headerPos = -this.HEADER_HEIGHT;
+      }
+    } else {
+      this.headerPos = 0;
+    }
+    this.startPos = currentPos;
+
+  }
+
+  onScrolla(): void {
+    console.log('test')
+  }
 }
---
+++ src/app/views/top/top.component.ts
@@ -0,0 +1,14 @@
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-top',
+  templateUrl: './top.component.html',
+  styleUrls: ['./top.component.scss']
+})
+export class TopComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
